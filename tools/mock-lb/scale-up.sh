845206bbcddada1ac679cb0b948182ffed49bc84
stavan-arrcus <stavan@arrcus.com>
2026-03-04 15:29:06 -0800

    Added scale-up script

--- tools/mock-lb/scale-up.sh
+++ tools/mock-lb/scale-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Scalable Launcher for vLLM Semantic Router
-# Usage: ./scale-up.sh <node_id> <host_port> <peer_address>
-# Example: ./scale-up.sh a 10000 192.168.64.3
+# Usage: ./scale-up.sh <node_id> <host_port> <health_port> <peer_address>
+# Example: ./scale-up.sh a 10000 8080 192.168.64.3
 
 if [ "$#" -ne 4 ]; then
     echo "Usage: ./scale-up.sh <node_id> <host_port> <health_port> <peer_address>"
@@ -29,15 +29,21 @@ if [ ! -f "config-$NODE_ID.yaml" ]; then
     cp config-a.yaml "config-$NODE_ID.yaml"
 fi
 
-# Pre-process Envoy config handles multi-line replacement
-export PEER_ENDPOINTS=$(printf "$PEER_ENDPOINTS")
-envsubst '${PEER_ENDPOINTS}' < envoy-template.yaml > "envoy-$NODE_ID.yaml"
+# Pre-process Envoy config using Python (since envsubst is often missing on Mac)
+export PEER_ENDPOINTS_VAL=$(printf "$PEER_ENDPOINTS")
+python3 -c "
+import os
+import sys
+content = sys.stdin.read()
+# Replace \${PEER_ENDPOINTS} with the environment variable value
+print(content.replace('\${PEER_ENDPOINTS}', os.environ.get('PEER_ENDPOINTS_VAL', '')))
+" < envoy-template.yaml > "envoy-$NODE_ID.yaml"
 
 # Launch using Docker Compose
 NODE_ID=$NODE_ID \
 HOST_PORT=$HOST_PORT \
 HEALTH_PORT=$HEALTH_PORT \
 ENVOY_CONFIG="envoy-$NODE_ID.yaml" \
-docker-compose -p "router-node-$NODE_ID" -f docker-compose.node.yaml up -d
+docker-compose -p "router-node-$NODE_ID" -f docker-compose.node.yaml up -d --build
 
 echo "Node $NODE_ID is running at http://localhost:$HOST_PORT"
